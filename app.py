422f9fa4729ca0f2222045dd6d9ad53ab5a99f27
pravar0709 <pravarupadhyay174@gmail.com>
2026-06-28 18:09:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,8 +10,18 @@ from tensorflow.keras.models import load_model
 from tensorflow.keras.applications.efficientnet import preprocess_input
 
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "*"}}, supports_credentials=False)
+@app.after_request
+def add_cors_headers(response):
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Access-Control-Allow-Methods'] = 'GET, POST, OPTIONS'
+    response.headers['Access-Control-Allow-Headers'] = 'Content-Type'
+    return response
 
+@app.route('/predict', methods=['POST', 'OPTIONS'])
+def handle_options():
+    if request.method == 'OPTIONS':
+        return '', 204
+        
 # --- ROBUST LOADING FIX ---
 # Strip 'quantization_config' at the Keras config level before deserialization
 original_dense_from_config = tf.keras.layers.Dense.from_config
