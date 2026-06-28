089926341bafa02bccc52e113c12a40d2556fc38
pravar0709 <pravarupadhyay174@gmail.com>
2026-06-28 17:47:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 import os
+os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"  
 import numpy as np
 from flask import Flask, request, jsonify
 from flask_cors import CORS
